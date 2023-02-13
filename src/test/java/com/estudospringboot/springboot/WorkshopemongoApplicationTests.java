1745aebf5671dd970ae55868276c64ed84df4cb4
Allisson Bezerra <allisson.geografia@gmail.com>
2023-02-13 10:46:28 -0300

    Primeiro Commit - Projeto Criado

---
+++ src/test/java/com/estudospringboot/springboot/WorkshopemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudospringboot.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
