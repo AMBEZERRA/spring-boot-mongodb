158c6503a11e13035b1c3d6c0663cbb3f28c4c5c
Allisson Bezerra <allisson.geografia@gmail.com>
2023-02-15 15:45:41 -0300

    Inserindo usuario com Post

--- src/main/java/com/estudospringboot/springboot/resources/UserResource.java
+++ src/main/java/com/estudospringboot/springboot/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.estudospringboot.springboot.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudospringboot.springboot.domain.User;
 import com.estudospringboot.springboot.dto.UserDTO;
@@ -37,4 +40,14 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public  ResponseEntity<Void> insert(@RequestBody UserDTO ObjDto){
+		User obj = service.fromDTO(ObjDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
 }
