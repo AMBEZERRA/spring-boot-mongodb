f0086c5fcc57c38b8a6d89885055a328a154fe9c
Allisson Bezerra <allisson.geografia@gmail.com>
2023-02-15 18:12:36 -0300

    Referenciando os posts do usuário

--- src/main/java/com/estudospringboot/springboot/domain/User.java
+++ src/main/java/com/estudospringboot/springboot/domain/User.java
@@ -1,9 +1,12 @@
 package com.estudospringboot.springboot.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")  // informando que essa classe faz parte de uma coleção do mongoDb
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy= true)
+	private List<Post> posts = new ArrayList<>();
 	
 	// List<Post> list = new Arrays<>(); ainda não pde fazer associação pq so tem essa classe
 	
@@ -56,6 +62,15 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
